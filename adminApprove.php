f1a42ab96b83c7183aff8c1fb386cb7409f77091
dynasys <dynasys.sol@gmail.com>
2015-11-18 21:31:51 -0500

    Create adminApprove.php

---
+++ adminApprove.php
@@ -0,0 +1,112 @@
+<!--<?php
+ session_start();
+    if(!isset($_SESSION['username'])){
+    header("Location:Login.php");
+    }
+?>-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>CS Connect</title>
+    <link rel="stylesheet" href="newStyles.css">
+    <style>
+        table {
+            width:100%;
+        }
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 5px;
+            text-align: left;
+        }
+        table#t01 tr:nth-child(even) {
+            background-color: #eee;
+        }
+        table#t01 tr:nth-child(odd) {
+            background-color:#fff;
+        }
+        table#t01 th	{
+            background-color: #782f40;
+            color: white;
+        }
+    </style>    
+</head>
+<body>
+    <header>
+        <img src="newSeal.png" alt="Florida State University Seal" width="189" height="81">
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="" title="Home" class="main">Home</a></li>
+            <li><a href="" title="Settings" class="main">Settings</a></li>
+        </ul>
+    </nav>
+
+    <div id="cover">
+    
+    <div id="infobar">
+        Welcome, Paul - Nieto
+        <a href="logout.php" title="Logout" class="logout">Logout</a>
+    </div>
+        
+    <div id="feed">
+          
+		<h2>Create New Company User</h2>
+		<h5>Enter the company's new username and password</h5>
+        <table id="t01">
+			<form method="post" action="" id="new_company" class="user">
+				<span>Username:</span>
+				<input type="text" name="username" id="username">
+				<br><span>Password:</span>
+				<input type="text" name="password" id="password">
+				<input type="Submit" value="submit" name="submit">
+            
+            <?php include "adminApproveNewScript.php" ?>
+			</form>
+            
+        </table>
+          
+    </div>
+    
+    <!--<div id="profileInfo">
+		<?php include "compInfoScript.php" ?>
+		
+        Paul - Nieto
+        <br>
+        Junior, Graduating Apr 2017 <br>
+        Computer Science
+        <br>
+        pn13b@my.fsu.edu <br>
+        resume last updated: tuba <br>
+        <br>
+        profile completion <br>
+        enter bar here
+    </div>-->
+        
+
+    
+    <div id="links">
+        <ul id="otherlist">
+            <li><a href="" title="CS Homepage" class="navaway">CS Homepage</a></li>
+            <li><a href="" title="Systems Group" class="navaway">Systems Group</a></li>
+            
+        </ul>
+    </div>
+        
+    <div class="clearfix"></div>
+        
+    </div>
+    
+    <footer>
+        <br>
+        © 2015 Florida State University <br>
+        600 W. College Avenue <br>
+        Tallahassee, FL 32306 <br>
+    </footer>
+        
+</body>
+</html>
