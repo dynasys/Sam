ec625005b484a0e11d5832f38b99274c0c3aa731
dynasys <dynasys.sol@gmail.com>
2015-11-21 16:31:42 -0500

    Update adminApproveDeleteScript.php

--- adminApproveDeleteScript.php
+++ adminApproveDeleteScript.php
@@ -1,21 +1,8 @@
 <?php
 if(isset($_POST['Approve'])){
-    $CID = $_GET['CID'];
-    //$CID = $_SESSION['companyID'];
-    //$Name = $_SESSION['compname'];
-    echo $CID;
-	echo "Approved ran";
-   
-	header("location: adminApprove.php");
-    /*$connection = mysql_connect("localhost", "root", "");
-    $db = mysql_select_db ("csconnect", $connection);
-    $sql = "UPDATE company
-            SET Approved='Y'
-            WHERE Company_ID=" . $CID;
-            
-    mysql_query($sql,$connection);
-    
-    mysql_close($connection);*/
+    $CID = $_POST['CID'];
+	
+	header("location: adminApprove.php?CID=".urlencode("$CID"));
 }
 if(isset($_POST['Delete'])){
     $CID = $_GET['CID'];
